3f20edc456c0344a1e14bff074d424d021fe1cff
Minha Kwon <namisoft@naver.com>
2023-03-03 15:23:07 +0900

    write screen

--- screens/Write.js
+++ screens/Write.js
@@ -1,13 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 import styled from "styled-components/native";
+import colors from "../colors";
 
-const View = styled.View``;
-const Text = styled.Text``;
+const View = styled.View`
+  flex: 1;
+  background-color: ${colors.bgColor};
+  padding: 0px 30px;
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 28px;
+  margin: 50px 0px;
+  text-align: center;
+  font-weight: 500;
+`;
+const TextInput = styled.TextInput`
+  background-color: white;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 18px;
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+`;
+const Btn = styled.TouchableOpacity`
+  width: 100%;
+  background-color: ${colors.btnColor};
+  margin-top: 20px;
+  padding: 10px 20px;
+  align-items: center;
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+`;
+const BtnText = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const Emotions = styled.View`
+  flex-direction: row;
+  margin-bottom: 20px;
+  justify-content: space-between;
+`;
+const Emotion = styled.TouchableOpacity`
+  background-color: white;
+  padding: 10px;
+  border-radius: 10px;
+  border-width: 1px;
+  border-color: ${(props) =>
+    props.selected ? "rgba(41,30,95,1)" : "transparent"};
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
+`;
+const EmotionText = styled.Text`
+  font-size: 14px;
+`;
+
+const emotions = ["🤯", "🥲", "🤬", "🤗", "🥰", "😊", "🤩"];
 
 const Write = () => {
+  const [selectedEmotion, setEmotion] = useState(null);
+  const [feelings, setFeelings] = useState("");
+  const onChangeText = (text) => setFeelings(text);
+  const onEmotionPress = (face) => setEmotion(face);
+  const onSubmit = () => {
+    if (feelings === "" || selectedEmotion === null) {
+      return Alert.alert("Please complete form.");
+    }
+  };
+  // console.log(feelings);
   return (
     <View>
-      <Text>Write</Text>
+      <Title>How do you fell now?</Title>
+      <Emotions>
+        {emotions.map((emotion, index) => (
+          <Emotion
+            key={index}
+            selected={emotion === selectedEmotion}
+            onPress={() => onEmotionPress(emotion)}
+          >
+            <EmotionText>{emotion}</EmotionText>
+          </Emotion>
+        ))}
+      </Emotions>
+      <TextInput
+        returnKeyType="done"
+        onSubmitEditing={onSubmit}
+        onChangeText={onChangeText}
+        value={feelings}
+        placeholder="Write your feelings..."
+      />
+      <Btn onPress={onSubmit}>
+        <BtnText>Save</BtnText>
+      </Btn>
     </View>
   );
 };
