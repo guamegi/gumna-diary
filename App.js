b9440a1e29f0930e00e482e08d0df324432748df
Minha Kwon <namisoft@naver.com>
2023-03-03 15:46:01 +0900

    realm 세팅

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
+import Realm from "realm";
+import AppLoading from "expo-app-loading";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: "gumnaDiaryDB",
+      schema: [FeelingSchema],
+    });
+    // console.log("realm:", realm);
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator />
