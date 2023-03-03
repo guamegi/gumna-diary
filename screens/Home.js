2349ce3cc535dc9e46b97b1eb89c1cb72bec5aec
Minha Kwon <namisoft@naver.com>
2023-03-03 13:05:44 +0900

    home screen

---
+++ screens/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../colors";
+
+const View = styled.View`
+  flex: 1;
+  padding: 0px 50px;
+  padding-top: 100px;
+  background-color: ${colors.bgColor};
+`;
+const Title = styled.Text`
+  color: ${colors.textColor};
+  font-size: 38px;
+  margin-bottom: 100px;
+`;
+const Btn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  width: 80px;
+  height: 80px;
+  background-color: ${colors.btnColor};
+  border-radius: 40px;
+  justify-content: center;
+  align-items: center;
+  elevation: 5;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+`;
+
+const Home = ({ navigation: { navigate } }) => {
+  return (
+    <View>
+      <Title>My journal</Title>
+      <Btn onPress={() => navigate("Write")}>
+        <Ionicons name="add" color="white" size={40} />
+      </Btn>
+    </View>
+  );
+};
+
+export default Home;
